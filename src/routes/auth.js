1bd0a173707f42353f133c7d3a258de35654b823
sivagurubilli <gurusrija123@gmail.com>
2023-01-07 14:03:12 +0530

    api updated

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, register } from "../controllers/auth.js";
+import { login, register } from "../controllers/auth";
 
 const router = express.Router();
 
